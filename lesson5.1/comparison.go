eaf88f464cf4b8cfd7cec6a06290afbeeaa5e0c5
aOPQAS <m94450188@gmail.com>
2025-02-14 21:49:12 +0300

    the program was partially completed, a loop for checking was added, and new outputs were added, but there is a small error in the output (new outputs appear)

--- lesson5.1/comparison.go
+++ lesson5.1/comparison.go
@@ -6,54 +6,57 @@ import (
 
 func compareCakes(oldRecipes, newRecipes Recipes) {
 
-	// delete cake if name does not match
+	// Add cake if names are the same
 	// ADDED cake "Moonshine Muffin"
 	for _, cake2 := range newRecipes.Cakes {
 		isAdded := true
 		for _, cake1 := range oldRecipes.Cakes {
 			if cake1.Name == cake2.Name {
 				isAdded = false
-				break
 			}
 		}
 		if isAdded {
-			fmt.Printf("ADDED cake \"%s\"\n", cake2.Name)
+			fmt.Println("ADDED cake", cake2.Name)
 		}
 	}
 
-	// REMOVED cake
+	// delete cake if name does not match
 	// REMOVED cake "Blueberry Muffin Cake"
 	for _, cake1 := range oldRecipes.Cakes {
 		isRemoved := true
 		for _, cake2 := range newRecipes.Cakes {
 			if cake1.Name == cake2.Name {
 				isRemoved = false
-				break
 			}
 		}
 		if isRemoved {
-			fmt.Printf("REMOVED cake \"%s\"\n", cake1.Name)
+			fmt.Println("REMOVED cake", cake1.Name)
 		}
 	}
 
-	// Checking the change in cooking time
+	// If the names of the cakes are different, they are deleted
 	// CHANGED cooking time for cake "Red Velvet Strawberry Cake" - "45 min" instead of "40 min"
 	for _, cake1 := range oldRecipes.Cakes {
 		for _, cake2 := range newRecipes.Cakes {
 			if cake1.Name == cake2.Name && cake1.Stovetime != cake2.Stovetime {
 				fmt.Printf("CHANGED cooking time for cake \"%s\" - \"%s\" instead of \"%s\"\n", cake1.Name, cake2.Stovetime, cake1.Stovetime)
+				break
 			}
 		}
 	}
 
-	// Compare ingredients
+	// If the names of the cakes are the same, then we move on to the ingredients and compare them
+	// Search for deleted and added ingredients
+	// Search for deleted and added ingredients
 	for _, cake1 := range oldRecipes.Cakes {
 		for _, cake2 := range newRecipes.Cakes {
 			if cake1.Name == cake2.Name {
+
 				oldIngredients := cake1.Ingredients
 				newIngredients := cake2.Ingredients
 
-				// ADDED ingredient
+				// Added ingredient
+				// ADDED ingredient "Coffee beans" for cake "Red Velvet Strawberry Cake"
 				for _, newIng := range newIngredients {
 					isAdded := true
 					for _, oldIng := range oldIngredients {
@@ -67,29 +70,23 @@ func compareCakes(oldRecipes, newRecipes Recipes) {
 					}
 				}
 
-				// REMOVED ingredient
+				// REMOVED ingredient "Vanilla extract" for cake "Red Velvet Strawberry Cake"
 				for _, oldIng := range oldIngredients {
 					isRemoved := true
 					for _, newIng := range newIngredients {
 						if oldIng.Itemname == newIng.Itemname {
 							isRemoved = false
-							break
-						}
-					}
-					if isRemoved {
-						fmt.Printf("REMOVED ingredient \"%s\" for cake \"%s\"\n", oldIng.Itemname, cake1.Name)
-					}
-				}
+							// // The unit of measurement for the ingredient has been changed.
+							// CHANGED unit for ingredient "Flour" for cake  "Red Velvet Strawberry Cake" - "mugs" instead of "cups"
+							if isRemoved {
+								fmt.Printf("REMOVED ingredient \"%s\" for cake \"%s\"\n", oldIng.Itemname, cake1.Name)
+							}
 
-				// CHANGED unit and count
-				for _, oldIng := range oldIngredients {
-					for _, newIng := range newIngredients {
-						if oldIng.Itemname == newIng.Itemname {
 							if oldIng.Itemunit != newIng.Itemunit {
-								// CHANGED unit for ingredient "Flour" for cake  "Red Velvet Strawberry Cake" - "mugs" instead of "cups"
 								fmt.Printf("CHANGED unit for ingredient \"%s\" for cake \"%s\" - \"%s\" instead of \"%s\"\n", oldIng.Itemname, cake1.Name, newIng.Itemunit, oldIng.Itemunit)
 								break
 							}
+							// The amount of ingredient has been changed
 							// CHANGED unit count for ingredient "Strawberries" for cake  "Red Velvet Strawberry Cake" - "8" instead of "7"
 							if oldIng.Itemcount != newIng.Itemcount {
 								fmt.Printf("CHANGED unit count for ingredient \"%s\" for cake \"%s\" - \"%s\" instead of \"%s\"\n", oldIng.Itemname, cake1.Name, newIng.Itemcount, oldIng.Itemcount)
@@ -97,19 +94,21 @@ func compareCakes(oldRecipes, newRecipes Recipes) {
 							}
 						}
 					}
+					// if isRemoved {
+					//   fmt.Printf("REMOVED ingredient \"%s\" for cake \"%s\"\n", oldIng.Itemname, cake1.Name)
+					// }
 				}
 
-				// Search for deleted ingredients (removed a unit of pieces)
-				// REMOVED unit "pieces" for ingredient "Cinnamon" for cake  "Red Velvet Strawberry Cake"
+				// REMOVED unit "pieces" for ingredient "Cinnamon" for cake "Red Velvet Strawberry Cake"
 				for _, oldIng := range oldIngredients {
-					isRemoved := true
+					unitRemoved := true
 					for _, newIng := range newIngredients {
 						if oldIng.Itemname == newIng.Itemname && oldIng.Itemunit == newIng.Itemunit {
-							isRemoved = false
+							unitRemoved = false
 							break
 						}
 					}
-					if isRemoved {
+					if unitRemoved {
 						fmt.Printf("REMOVED unit \"%s\" for ingredient \"%s\" for cake \"%s\"\n", oldIng.Itemunit, oldIng.Itemname, cake1.Name)
 					}
 				}
